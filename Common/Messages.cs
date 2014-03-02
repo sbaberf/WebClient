5c7f700b2931353693221b892b651fa1805bee9f
Baber Sheikh <i090213@nu.edu.pk>
2014-03-02 12:24:33 +0500

    Allure First Commit
    Features:
    - Sending
    - Recieving
    - Drawing Board
    - Recive Cordinates
    - Send Cordinates

---
+++ Common/Messages.cs
@@ -0,0 +1,14 @@
+﻿namespace WebClient.Common
+{
+    /*
+     * This example class shows how complete objects can be tranmitted with SingnalR.
+     */
+    public class Messages
+    {
+        public string Body;
+        public string From;
+        public string To;
+        public string Type;
+
+    }
+}
